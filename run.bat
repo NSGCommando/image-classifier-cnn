36d162f10ca10587bc374719ed4fa9057b23bc52
NSGCommando <41550763+NSGCommando@users.noreply.github.com>
2026-03-27 12:38:26 +0530

    Dockerization Refactor - Add 'dockerfile' to 'src/', 'docker-compose.yml' and '.dockerignore' to root - Run server instance via Docker Compose Up - Move 'run.py' to 'src/' for full API isolation - Update 'README.md' and 'run.bat'

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo off
 set SCRIPT_LOC=%~dp0
+set PYTHONPATH=%SCRIPT_LOC%
 call "%SCRIPT_LOC%.venv\Scripts\activate.bat"
-python "%SCRIPT_LOC%run.py" %*
+python "%SCRIPT_LOC%\src\run.py" %*
 REM: If there is any error, pause the window to allow reading what went wrong
 if %errorlevel% neq 0 pause
--- src/run.py
+++ src/run.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         print("Running as API Server!")
         uvicorn.run(
             "src.app:app", 
-            host="127.0.0.1", 
+            host="0.0.0.0", 
             port=8000, 
             reload=False
         )
