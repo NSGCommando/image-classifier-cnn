e88f33156dfc6ca41ce7c4fc12da6107fe623e4c
NSGCommando <41550763+NSGCommando@users.noreply.github.com>
2026-04-17 21:24:49 +0530

    CLI Zip handling and Docker Update - Added Zip Handling to CLI mode - Refactored entryoint flags from 'image' to 'file, folder'   - 'file' flag is for files (zips and images)   - 'folder' flag is for lcoal directories - Add Inference only requirements file, trimmed down from project requirements.txt - Update .dockerignore to ignore unnecessary .keras and .pb model files

--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
 import uvicorn
 from pathlib import Path
-from src.utils.util_functions import default_parser
+from src.utils.util_functions_inference import default_parser
 from src.scripts.cli import cli_inference
 # Run this for the CLI mode
 
@@ -8,9 +8,17 @@ if __name__ == "__main__":
     
     args = default_parser()
 
-    if args.image:
-        user_path = Path(args.image)
-        cli_inference(user_path)
+    if args.file:
+        print("Running as file inference!")
+        user_path = Path(args.file)
+        if not user_path.is_file():raise TypeError("Argument for --file flag must be a path to an image or zip")
+        cli_inference(filepath=user_path,filetype="file")
+    
+    elif args.folder:
+        print("Running as local directory inference!")
+        user_path = Path(args.folder)
+        if user_path.is_file():raise TypeError("Argument for --folder flag must be a folder/directory path")
+        cli_inference(filepath=user_path,filetype="folder")
 
     elif args.server:
         print("Running as API Server!")
