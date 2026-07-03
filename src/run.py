01f6f695b4924cbe527c28463efd6cb6db17d996
NSGCommando <41550763+NSGCommando@users.noreply.github.com>
2026-07-03 13:42:22 +0530

    Dispatcher-Worker Update for Docker - Add section in 'docker-compose.yml' for Redis container start up and   health chech - Update 'dockerfile' to explicitly copy only required files during   final build step - reduce reliance on '.dockerignore' and chances of   missed bloat files - Update 'dockerfile' run command to use integer arg with '--server'   (start 2 workers with '2') - Update 'run.py' to remove HTTP thread starter and add Redis client   orchestrator and inference worker startup helper 'start_workers'

--- src/run.py
+++ src/run.py
@@ -1,13 +1,25 @@
-import uvicorn, threading
+import threading, asyncio, logging
 from pathlib import Path
 from src.utils.util_functions_inference import default_parser, run_server
 from src.scripts.cli import cli_inference
+from src.scripts.inference_worker import worker_wrapper
+from src.utils.redis_client import create_worker_group, get_redis
+from src.utils.logging_config import configure_logging
+
+configure_logging()
+logger = logging.getLogger(__name__)
 
 dispatch_server_cmd = ["uvicorn", "src.scripts.dispatcher:dispatcher", "--host", "0.0.0.0", "--port", "8000"]
-inference_server_cmd = ["uvicorn", "src.scripts.http_server:http_server", "--host", "0.0.0.0", "--port", "8001"]
+# inference_server_cmd = ["uvicorn", "src.scripts.http_server:http_server", "--host", "0.0.0.0", "--port", "8001"]
 
-if __name__ == "__main__":
-    
+async def start_workers(redis_client,worker_count:int):
+    tasks = [
+        asyncio.create_task(worker_wrapper(redis_client=redis_client,worker_id=i))
+        for i in range(worker_count)
+    ]
+    await asyncio.gather(*tasks)
+
+async def run_app():
     args = default_parser()
 
     if args.file:
@@ -23,15 +35,16 @@ if __name__ == "__main__":
         cli_inference(filepath=user_path,filetype="folder")
 
     elif args.server:
+        # start dispatcher thread
         thread1 = threading.Thread(target=run_server,args=(dispatch_server_cmd,"dispatch_server"), daemon=True)
-        thread2 = threading.Thread(target=run_server,args=(inference_server_cmd,"inference_server"), daemon=True)
         thread1.start()
-        thread2.start()
+        logger.info("Dispatcher Startup Checkpoint")
+            
+        # create redis worker tasks and the redis worker group
+        redis_client_orchestrator = await get_redis("orchestrator")
+        await create_worker_group(redis_client=redis_client_orchestrator)
+        await start_workers(redis_client_orchestrator,int(args.server))
 
-        try:
-            # Keep main thread alive while servers run
-            while thread1.is_alive() or thread2.is_alive():
-                thread1.join(timeout=1)
-                thread2.join(timeout=1)
-        except KeyboardInterrupt:
-            print("Main orchestrator caught Ctrl+C, shutting down all servers...")
+
+if __name__ == "__main__":
+    asyncio.run(run_app())
